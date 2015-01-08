c476577de9ef13c2cc59bae6a15e0cfb36f26e31
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2015-01-08 12:18:58 +0530

    added some comments

--- _8x8RedLED.ino
+++ _8x8RedLED.ino
@@ -3,13 +3,20 @@
   
   Author: Dhruv Joshi
   
-  This is being made expressly for the FPM project
+  This is being made expressly for the FPM project.
+  8x8 red LED display was procured from local vendors.
+  
+  Teensy 3.1 used as the controller.
+  Digital pins 5-12 and 13-21 used respectively for the 
+  -ve and +ve terminals
 
 *******************************************/
 void setup() {
+  Serial.begin(9600);
+  
   
 }
 
 void loop() {
-  
+   
 }
